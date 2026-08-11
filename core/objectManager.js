b18ab2d74a1fd68654452d165ff26f85c80d1ccd
Iammdtanvirrahman2007 <mdtanvirrahman2021a@gmail.com>
2026-08-11 17:44:13 +0600

    Centralize scene object registration and cleanup

--- core/objectManager.js
+++ core/objectManager.js
@@ -1,23 +1,61 @@
 const objects = [];
 
+function isEditorObject(object) {
+    return !!object && (
+        object.userData?.editorObject === true ||
+        object.userData?.selectable === true ||
+        object.isMesh ||
+        object.isGroup
+    );
+}
+
 export function addObject(scene, object) {
-    scene.add(object);
-    objects.push(object);
+    if (!scene || !object) return null;
+
+    if (!scene.children.includes(object)) scene.add(object);
+    object.userData.editorObject = true;
+
+    if (!objects.includes(object)) objects.push(object);
+    return object;
 }
 
 export function removeObject(scene, object) {
-    scene.remove(object);
+    if (!object) return;
+
+    if (object.parent) object.parent.remove(object);
+
     const index = objects.indexOf(object);
-    if (index !== -1) {
-        objects.splice(index, 1);
-    }
+    if (index !== -1) objects.splice(index, 1);
 }
 
 export function getObjects() {
     return objects;
 }
 
-// এই ফাংশনটি Load করার সময় পুরোনো লিস্ট ক্লিয়ার করার জন্য দরকার
-export function clearObjects() {
+export function getObjectByUUID(uuid) {
+    return objects.find(object => object.uuid === uuid) || null;
+}
+
+export function clearObjects(scene) {
+    if (scene) {
+        [...objects].forEach(object => {
+            if (object.parent) object.parent.remove(object);
+        });
+    }
+
     objects.length = 0;
+}
+
+export function syncObjects(scene) {
+    objects.length = 0;
+    if (!scene) return objects;
+
+    scene.traverse(object => {
+        if (object === scene) return;
+        if (isEditorObject(object) && (object.isMesh || object.isGroup || object.userData?.selectable)) {
+            if (!objects.includes(object)) objects.push(object);
+        }
+    });
+
+    return objects;
 }
